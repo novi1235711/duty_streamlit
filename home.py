ded2c380a23928ec8e208bfda6012145d640b208
novi1235711 <novi1235711@gmail.com>
2023-09-14 18:27:53 +0900

    4th commit

--- home.py
+++ home.py
@@ -42,6 +42,8 @@ df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
 hds_arr =  holidayslist[1:]
 namelist1 = df_names['1年目名簿']
 namelist2 = df_names['2年目名簿']
+timelist1 = df_A1st['Time']
+timelist2 = df_A2nd['Time']
 dtnow = datetime.datetime.now()
 now = dtnow.astimezone(timezone('Asia/Tokyo')).strftime('%Y/%m/%d %H:%M:%S')
 year = int(dtnow.strftime('%Y'))
@@ -58,7 +60,7 @@ def page_home():
     """
     ##### 回答フォームは左上のメニューから該当のフォームで回答してください。
     ##### (スマホで回答する方は少し上にスクロールして出てくる＞をタップして)
-    #### ＊くれぐれも、絶対に！他人の名前で回答しないようにお願いします。（上書きされます）
+    #### ＊くれぐれも、他人の名前で回答しないようにお願いします。
     #####
     ##### 日当直決め担当者は以下の要領で行ってください。
     ##### 1．左上のメニューから管理者ページにアクセスし、祝日の登録、名簿の確認を行って下さい。
@@ -69,8 +71,34 @@ def page_home():
     """
     ### ＊回答状況＊
     """
-    st.write(df_A1st)
-    st.write(df_A2nd)
+    st.write("間違いや再回答をした場合は、以下の履歴を参考に回答を削除してください。")
+    st.write("＊トラブル回避のため自動的な上書き機能は廃止されました。また、1つずつしか削除できないようにしています。")
+    df_A1st = pd.DataFrame(A1stlist[1:],columns=A1stlist[0])
+    with st.form("delete1"):
+        st.write(df_A1st)
+        id1 = st.selectbox("注意して、削除したい回答の時間を選択してください。",timelist1)
+        deleted = st.form_submit_button("Delete")
+        if deleted:
+            df_A1st = pd.DataFrame(A1stlist[1:],columns=A1stlist[0])
+            df_A1st = df_A1st[df_A1st['Time']!=id1]
+            A1st.clear()
+            set_with_dataframe(A1st,df_A1st,row=1,col=1)
+            st.write("選択したデータを削除しました。")
+            st.write("＊もう一度削除する際は、必ずページを再読み込みしてください。")
+            st.write(df_A1st) 
+    df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
+    with st.form("delete2"):
+        st.write(df_A2nd)
+        id2 = st.selectbox("注意して、削除したい回答の時間を選択してください。",timelist2)
+        deleted = st.form_submit_button("Delete")
+        if deleted:
+            df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
+            df_A2nd = df_A2nd[df_A2nd['Time']!=id2]
+            A2nd.clear()
+            set_with_dataframe(A2nd,df_A2nd,row=1,col=1)
+            st.write("選択したデータを削除しました。")
+            st.write("＊もう一度削除する際は、必ずページを再読み込みしてください。")
+            st.write(df_A2nd)
     
 
 def page_form1():
@@ -161,16 +189,15 @@ def page_form1():
             st.write("Name: ", yourname)
             st.write('Your comment: ', comment)
             st.write('You selected: ', NG_list1, NG_list2)
-            st.write("上記の内容で提出しました")
+            st.write("上記の内容で提出しました。")
             df_A1st = pd.DataFrame(A1stlist[1:],columns=A1stlist[0])
             data1 = [[now,yourname,comment,NG_list1,NG_list2,demandsA1,demandsB1]]
             columns1 = ['Time','Name','Comment','NG1','NG2','OK1','OK2']
             nd = pd.DataFrame(data=data1,columns=columns1)
-            df_A1st = df_A1st[df_A1st['Name']!=yourname]
             df_A1st = pd.concat([df_A1st,nd],axis=0,)
             set_with_dataframe(A1st,df_A1st,row=1,col=1)
-            st.write(df_A1st)
-    
+            st.write(df_A1st) 
+        
 
 def page_form2():
     st.title('2年目_日当直回答フォーム')
@@ -265,7 +292,6 @@ def page_form2():
             data2 = [[now,yourname,comment,NG_list1,NG_list2,demandsA2,demandsB2]]
             columns2 = ['Time','Name','Comment','NG1','NG2','OK1','OK2']
             nd = pd.DataFrame(data=data2,columns=columns2)
-            df_A2nd = df_A2nd[df_A2nd['Name']!=yourname]
             df_A2nd = pd.concat([df_A2nd,nd],axis=0,)
             set_with_dataframe(A2nd,df_A2nd,row=1,col=1)
             st.write(df_A2nd)
